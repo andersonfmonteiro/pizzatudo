404f0177655e2160612b24f05da781a70d8456f3
Anderson Fonseca Monteiro <andersonfmonteiro@gmail.com>
2023-05-29 09:46:07 -0300

    primeiro

---
+++ src/pages/PaymentSuccess/index.jsx
@@ -0,0 +1,12 @@
+import { Alert } from "../../components/elements/Alert";
+const PaymentSuccess = () => {
+    return (
+        <div className="max-w-lg mx-auto p-4">
+            <Alert variant="success">
+                Seu Pagamento foi Confirmado
+            </Alert>
+        </div>
+    )
+}
+
+export default PaymentSuccess;
